7afb3837bbd53fdaf30e28f17bd1ab9d99128ad4
lbvd <59065245+lbvd@users.noreply.github.com>
2019-12-19 20:15:35 +0100

    Update stat.sh

--- stat.sh
+++ stat.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-sudo python3 /home/ice/Desktop/at-own_aplication/at_start.py
+# It is useful to place this file on the Desktop (of your linux machine) because it allows you to start the application without
+# any detours.
+#How to do?
+#Enter the path to the location where the script files are stored
+#You may also change the python script this allows you to start directly the gui or the shell application
+#(for example replace at_start.py with atg.py)
+#press Run in Terminal  
+
+sudo python3 /home/user/Desktop/folder/at_start.py
